d3d69b3a4da85891ddf90206c0c05d51c058d5af
Rishabh <rishabh667788@gmail.com>
2025-02-01 22:18:37 +0530

    vite & script
    data generation & vite config

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,130 @@
+import { DefaultTheme, defineConfig, HeadConfig } from "vitepress";
+import { PACKAGES, packageSidebars } from "./packages.config";
+import { DESCRIPTION, IMAGE, LOCALE, TITLE } from "./meta.config";
+import { NAVIGATION } from "./nav.config";
+import lightbox from "vitepress-plugin-lightbox";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+	title: TITLE,
+	description: DESCRIPTION,
+
+	srcDir: "src",
+
+	base: "/docs/",
+
+	head: [
+		["link", { rel: "icon", href: "/docs/favicon.ico" }],
+		["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+		["link", { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" }],
+		["link", { href: "https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap", rel: "stylesheet" }],
+	],
+
+	cleanUrls: true,
+
+	outDir: "./dist",
+
+	sitemap: {
+		hostname: "https://os.prozilla.dev/docs/",
+	},
+
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: "Demo", link: "https://os.prozilla.dev/", target: "_blank" },
+			...NAVIGATION.map(({ text: navigationText, base, items = [] }) => ({
+				text: navigationText,
+				base,
+				items: items.map(({ text, link }) => ({
+					text,
+					link: base ? base + link : link,
+				})),
+			}) as DefaultTheme.NavItem),
+		],
+
+		sidebar: Object.assign(
+			{},
+			{
+				"/": NAVIGATION as DefaultTheme.SidebarItem[],
+				...packageSidebars(PACKAGES),
+			}
+		),
+
+		editLink: {
+			pattern: "https://github.com/prozilla-os/ProzillaOS/edit/main/docs/src/:path",
+			text: "Suggest changes to this page",
+		},
+
+		socialLinks: [
+			{ icon: "github", link: "https://github.com/prozilla-os/ProzillaOS" },
+			{ icon: "discord", link: "https://discord.gg/JwbyQP4tdz" },
+			{ icon: "npm", link: "https://www.npmjs.com/package/prozilla-os" },
+		],
+
+		logo: {
+			dark: "/logo-light.svg",
+			light: "/logo-dark.svg",
+		},
+
+		siteTitle: "ProzillaOS",
+		
+		footer: {
+			message: "Built by <strong><a href=\"https://prozilla.dev/\" target=\"_blank\">Prozilla</a></strong>",
+			copyright: "Copyright &copy; 2023-present Prozilla",
+		},
+
+		search: {
+			provider: "local",
+			options: {
+				detailedView: true,
+			},
+		},
+	},
+
+	transformPageData(pageData) {
+		pageData.frontmatter.head ??= [] as HeadConfig[];
+		const head = pageData.frontmatter.head as HeadConfig[];
+
+		const title = pageData.frontmatter.layout === "home"
+			? TITLE
+			: `${pageData.title} | ${TITLE}`;
+		head.push(["meta", { name: "og:title", content: title }]);
+		head.push(["meta", { name: "twitter:title", content: title }]);
+
+		const description = pageData.frontmatter.description as string ?? DESCRIPTION;
+		head.push(["meta", { name: "og:description", content: description }]);
+		head.push(["meta", { name: "twitter:description", content: description }]);
+
+		const canonicalUrl = `https://os.prozilla.dev/docs/${pageData.relativePath}`
+			.replace(/index\.md$/, "")
+			.replace(/\.(md|html)$/, "");
+		head.push(["link", { rel: "canonical", href: canonicalUrl }]);
+		head.push(["meta", { name: "og:url", content: canonicalUrl }]);
+		head.push(["meta", { name: "twitter:url", content: canonicalUrl }]);
+
+		const locale = LOCALE;
+		head.push(["meta", { name: "og:locale", content: locale }]);
+
+		const image = pageData.frontmatter.image as string ?? IMAGE;
+		head.push(["meta", { name: "og:image", content: image }]);
+		head.push(["meta", { name: "twitter:image", content: image }]);
+
+		// Other meta data
+		head.push(["meta", { name: "og:type", content: "website" }]);
+		head.push(["meta", { name: "twitter:card", content: "summary_large_image" }]);
+	},
+
+	ignoreDeadLinks: [
+		/^https?:\/\/localhost/,
+	],
+
+	markdown: {
+		theme: {
+			dark: "material-theme",
+			light: "material-theme-lighter",
+		},
+		config: (markdown) => {
+			markdown.use(lightbox, {});
+		},
+	},
+});
---
+++ docs/.vitepress/packages.config.ts
@@ -0,0 +1,238 @@
+import { DefaultTheme } from "vitepress";
+
+export interface PackageData {
+	text: string;
+	link: string;
+	items?: DefaultTheme.SidebarItem[];
+	category: "Bundle" | "Libraries" | "Apps";
+};
+
+export const PACKAGES: PackageData[] = [
+	{
+		text: "prozilla-os",
+		link: "prozilla-os",
+		category: "Bundle",
+	},
+	{
+		text: "@prozilla-os/core",
+		link: "core",
+		category: "Libraries",
+		items: [
+			{
+				text: "Classes",
+				base: "/reference/core/classes",
+				collapsed: false,
+				items: [
+					{
+						text: "Apps",
+						base: "/reference/core/classes/apps",
+						collapsed: true,
+						items: [
+							{ text: "App", link: "/app" },
+						],
+					},
+					{
+						text: "System",
+						base: "/reference/core/classes/system",
+						collapsed: true,
+						items: [
+							{ text: "AppsConfig", link: "/apps-config" },
+							{ text: "DesktopConfig", link: "/desktop-config" },
+							{ text: "MiscConfig", link: "/misc-config" },
+							{ text: "ModalsConfig", link: "/modals-config" },
+							{ text: "TaskbarConfig", link: "/taskbar-config" },
+							{ text: "TrackingConfig", link: "/tracking-config" },
+							{ text: "VirtualDriveConfig", link: "/virtual-drive-config" },
+							{ text: "WindowsConfig", link: "/windows-config" },
+						],
+					},
+					{
+						text: "Utils",
+						base: "/reference/core/classes/utils",
+						collapsed: true,
+						items: [
+							{ text: "TimeManager", link: "/time-manager" },
+							{ text: "Vector2", link: "/vector2" },
+						],
+					},
+					{
+						text: "Virtual Drive",
+						base: "/reference/core/classes/virtual-drive",
+						collapsed: true,
+						items: [
+							{ text: "VirtualBase", link: "/virtual-base" },
+							{ text: "VirtualFolder", link: "/virtual-folder" },
+						],
+					},
+				],
+			},
+			{
+				text: "Constants",
+				link: "/constants",
+			},
+			{
+				text: "Functions",
+				base: "/reference/core/functions",
+				collapsed: false,
+				items: [
+					{ text: "Browser", link: "/browser" },
+					{ text: "Keyboard", link: "/keyboard" },
+					
+				],
+			},
+			{
+				text: "Hooks",
+				base: "/reference/core/hooks",
+				collapsed: false,
+				items: [
+					{ text: "useClassNames", link: "/use-class-names" },
+					{ text: "useStaticClassName", link: "/use-static-class-name" },
+					{ text: "useSystemManager", link: "/use-system-manager" },
+				],
+			},
+		],
+	},
+	{
+		text: "@prozilla-os/skins",
+		link: "skins",
+		category: "Libraries",
+		items: [
+			{
+				text: "Classes",
+				base: "/reference/skins/classes",
+				collapsed: false,
+				items: [
+					{ text: "Skin", link: "/skin" },
+				],
+			},
+		],
+	},
+	{
+		text: "@prozilla-os/shared",
+		link: "shared",
+		category: "Libraries",
+		items: [
+			{
+				text: "Classes",
+				base: "/reference/shared/classes",
+				collapsed: false,
+				items: [
+					{ text: "EventEmitter", link: "/event-emitter" },
+				],
+			},
+			{
+				text: "Constants",
+				base: "/reference/shared/constants",
+				collapsed: false,
+				items: [
+					{ text: "ANSI", link: "/ansi" },
+				],
+			},
+			{
+				text: "Functions",
+				base: "/reference/shared/functions",
+				collapsed: false,
+				items: [
+					{ text: "Array", link: "/array" },
+					{ text: "Date", link: "/date" },
+					{ text: "Math", link: "/math" },
+					{ text: "Number", link: "/number" },
+				],
+			},
+		],
+	},
+	{
+		text: "@prozilla-os/dev-tools",
+		link: "dev-tools",
+		category: "Libraries",
+		items: [
+			{
+				text: "Functions",
+				base: "/reference/dev-tools/functions",
+				collapsed: false,
+				items: [
+					{ text: "Console", link: "/console" },
+					{ text: "Vite", link: "/vite" },
+				],
+			},
+		],
+	},
+	{
+		text: "@prozilla-os/calculator",
+		link: "apps/calculator",
+		category: "Apps",
+	},
+	{
+		text: "@prozilla-os/file-explorer",
+		link: "apps/file-explorer",
+		category: "Apps",
+	},
+	{
+		text: "@prozilla-os/media-viewer",
+		link: "apps/media-viewer",
+		category: "Apps",
+	},
+	{
+		text: "@prozilla-os/settings",
+		link: "apps/settings",
+		category: "Apps",
+	},
+	{
+		text: "@prozilla-os/terminal",
+		link: "apps/terminal",
+		category: "Apps",
+	},
+	{
+		text: "@prozilla-os/text-editor",
+		link: "apps/text-editor",
+		category: "Apps",
+	},
+	{
+		text: "@prozilla-os/logic-sim",
+		link: "apps/logic-sim",
+		category: "Apps",
+	},
+];
+
+export const packageSidebars = (packages: PackageData[]): DefaultTheme.Sidebar => {
+	const sidebar: DefaultTheme.Sidebar = {};
+
+	packages.forEach(({ text, link, items = [] }) => {
+		const base = `/reference/${link}`;
+		sidebar[base] = {
+			base,
+			items: [
+				{
+					text,
+					items: [
+						{ text: "Info", link: "/" },
+						...items,
+					],
+				},
+			],
+		};
+	});
+
+	return sidebar;
+};
+
+export const packageReferenceItems = (packages: PackageData[]): DefaultTheme.SidebarItem[] => {
+	const categories: Record<string, DefaultTheme.SidebarItem> = {};
+
+	packages.forEach(({ text, link, category }) => {
+		if (!Object.keys(categories).includes(category)) {
+			categories[category] = {
+				text: category,
+				collapsed: true,
+				items: [],
+			};
+		}
+
+		categories[category].items?.push({
+			text,
+			link: "/" + link,
+		});
+	});
+
+	return Object.values(categories);
+};
