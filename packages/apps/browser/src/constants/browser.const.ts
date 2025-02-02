6b60bcee638777effd0113ca93969d05c73dba52
Rishabh <rishabh667788@gmail.com>
2025-02-02 09:40:24 +0530

    Update browser.const.ts

--- packages/apps/browser/src/constants/browser.const.ts
+++ packages/apps/browser/src/constants/browser.const.ts
@@ -1,2 +1,2 @@
-export const HOME_URL = "https://www.google.com/search?igu=";
+export const HOME_URL = "https://www.google.com/search?igu=1";
 export const SEARCH_URL = "https://www.google.com/search?igu=1";
