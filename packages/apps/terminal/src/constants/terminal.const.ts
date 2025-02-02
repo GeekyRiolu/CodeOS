857d37a5a1769e65b99444dee8675b1314b4b9c3
Rishabh <rishabh667788@gmail.com>
2025-02-02 20:03:33 +0530

    Update terminal.const.ts

--- packages/apps/terminal/src/constants/terminal.const.ts
+++ packages/apps/terminal/src/constants/terminal.const.ts
@@ -1,10 +1,10 @@
 import { ANSI } from "@prozilla-os/shared";
 
 export const USERNAME = "user";
-export const HOSTNAME = "prozilla-os";
+export const HOSTNAME = "code-os";
 export const MAX_WIDTH = 50;
 
-export const WELCOME_MESSAGE = `${ANSI.fg.cyan + ANSI.decoration.dim}$APP_NAME - Made by Prozilla${ANSI.reset}`
+export const WELCOME_MESSAGE = `${ANSI.fg.cyan + ANSI.decoration.dim}$APP_NAME - Made by CodeOS${ANSI.reset}`
 	+ `\n${ANSI.decoration.dim}Type 'help' for a list of commands.${ANSI.reset}\n`;
 
 export const ASCII_LOGO = `
